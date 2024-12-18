916906210d7bdd467b548c6ddfd75472cf735896
Likhitha sree <likhithasree1109@gmail.com>
2024-12-18 10:10:44 +0530

    Add files via upload

---
+++ js53.js
@@ -0,0 +1,11 @@
+str1="nipuna"
+str2="ONGOLE"
+str3="state bank of india"
+str4="     ongole       "
+console.log("character at="+str1.charAt(1))
+console.log("Upper case=",+str1.toUppercase())
+console.log("low case=",+str2.toLowercase())
+console.log("trim="+str4.trim())
+console.log("substring="+str3.substring(0,5))
+console.log("starts with="+str3.starswith("bri"))
+console.log("replace="+str3.replace('a','x'))
---
+++ js54.js
@@ -0,0 +1,22 @@
+d=date()
+console.log("Date=",d)
+
+
+d=new Date()
+m=d.getMonth()+1
+console.log(m)
+
+
+d=new Date()
+y=d.getFullYear()
+console.log(y)
+
+
+d=new Date()
+x=d.getDay()+1
+console.log(x)
+
+
+d=new Date()
+h=d.getHours()
+console.log(h)
