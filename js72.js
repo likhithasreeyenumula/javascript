4e1734933642233c11d08798e2b8054170c5711f
Likhitha sree <likhithasree1109@gmail.com>
2024-12-28 11:03:43 +0530

    Add files via upload

---
+++ js72.js
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>72</title>
+    </head>
+    <body>
+        <script text="text/javascript">
+            function validateEmail(input Text)
+            {
+                var mailFormat=/n\w+(!.-]?\wt)*@\w+([\.-?\w+)*(\.\w{2,3})+$/;
+                if(inputText.value match(mailFormat))
+                {
+                    alert("valid email address");
+                    document fm1.fname.focus();
+                    return true;
+                }
+                else
+                {
+                    alert("you have entered in valid email address!");
+                    document.fm1.fname.focus();
+                    return false;
+                }
+            }
+        </script>
+    </body>
+</html>
