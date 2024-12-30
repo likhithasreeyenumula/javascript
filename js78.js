f6d81e95b0b1f3182d2c005a5a96d15f65bd1c5f
Likhitha sree <likhithasree1109@gmail.com>
2024-12-30 10:26:13 +0530

    Add files via upload

---
+++ js78.js
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>
+            78
+        </title>
+    </head>
+    <body>
+        <h1>javascript Function</h1>
+        <h2>Function sequence</h2>
+        <p>javascript function are executed in the sequence they are called</p>
+        <p id="demo"></p>
+        <script>
+            function mycalculator(num1,num2){
+                let sum=num1+num2;
+                myDisplayer(sum);
+            }
+            mycalculator(5,5);
+        </script>
+    </body>
+</html>
