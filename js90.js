376b1acc7b905c0fe366365d37445b45f57901ed
Likhitha sree <likhithasree1109@gmail.com>
2025-01-03 09:31:27 +0530

    Add files via upload

---
+++ js90.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>90</title>
+        <script src="file://d://softwares/jquery-3.7.1.js"></script>
+        <script type="text/javascript" language="javascript">
+        $(function(){
+            $("p").css("color","pink");
+        });
+        </script>
+    </head>
+    <body>
+    <h5>jquery Example</h5>
+        <p>1 st program</p>
+        <p>2 nd program</p>
+        <p>3 rd program</p>
+    </body>
+</html>
