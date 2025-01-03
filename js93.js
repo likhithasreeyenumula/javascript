8a55134e5b39041f1e53a3a1b2e2a7e739dc3912
Likhitha sree <likhithasree1109@gmail.com>
2025-01-03 18:51:35 +0530

    Add files via upload

---
+++ js93.js
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>93</title>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script>
+            $(document).ready(function(){
+                $("#hide").click(function(){
+                    $("p").hide();
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <p><b>This is a pome</b>
+        Twinkle,Twinkle,little<br/></p>
+        <button id="hide">Hide</button>
+    </body>
+</html>
