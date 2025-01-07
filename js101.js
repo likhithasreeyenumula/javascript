b6368f298430122f46f142b9ae002c991a412f15
Likhitha sree <likhithasree1109@gmail.com>
2025-01-07 11:12:12 +0530

    Add files via upload

---
+++ js101.js
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>101</title>
+        <script src="js link"></script>
+        <script>
+            $(document).ready(function(){
+                $("#flip").click(function(){
+                    $("#panel").slideup("slow");
+                });
+            });
+        </script>
+        <style>
+            #panel,#flip{
+                padding:5px;
+                text-align:center;
+                background-color:#00FFFF;
+                border:solid 1px#c3c3c3;
+            }
+            #panel{
+                padding:50px;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="flip">click to slideup</div>
+        <div id="panel">HELLO</div>
+    </body>
+</html>
