2e512b56eb689b47d91bf63cfd0b497e26432b07
Likhitha sree <likhithasree1109@gmail.com>
2025-01-07 10:11:48 +0530

    Add files via upload

---
+++ js97.js
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>
+            97
+        </title>
+        <script src="js link"></script>
+        <script>
+            $document.ready(function(){
+                $("button").click(function(){
+                    $("div.d1").toggle();
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <div class="d1" style="border:1px solidblack;padding:10px;width:250px">
+            <p><b>This is a little poem</b><br/>
+            Twinkle,Twinkle,little star<br/>
+            how I wonder what you are</p>
+        </div>
+    </body>
+</html>
