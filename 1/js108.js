4c24821b0ac970a17c2132d14230d655a696fd93
Likhitha sree <likhithasree1109@gmail.com>
2025-01-08 19:21:07 +0530

    Add files via upload

---
+++ 1/js108.js
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>108</title>
+        <script>
+            $(document).ready(function (){
+              $("button").click(function(){
+                $("p").text("welcome");
+              })  ;
+            });
+        </script>
+    </head>
+    <body>
+        <button>click</button>
+        <p>para1</p>
+        <p>para2</p>
+        <p>para3</p>
+    </body>
+</html>
