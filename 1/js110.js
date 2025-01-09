955e96c120fca8614f89984d8ab26a66a940c7f3
Likhitha sree <likhithasree1109@gmail.com>
2025-01-09 09:22:49 +0530

    Add files via upload

---
+++ 1/js110.js
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>110</title>
+        <script src=""></script>
+        <script>
+            $(document).ready(function(){
+                $("button").click(function(){
+                    alert("...........");
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <h2>This is a heading</h2>
+        <p style="background-color:#ff0000">The backgroundcolor of this paragraph is red</p>
+        <button>click</button>
+    </body>
+</html>
