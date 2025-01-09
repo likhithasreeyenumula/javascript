dbc757fec589e8d465f4e9dfdf54049ec26eec15
Likhitha sree <likhithasree1109@gmail.com>
2025-01-09 09:24:08 +0530

    Add files via upload

---
+++ 1/js113.js
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <tiltle>113</tiltle>
+        <script src=""></script>
+        <script>
+            $(document).ready(function(){
+                $("p").bind("click",function(){
+                    alert("This para is clicked");
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <p>Click on the statement</p>
+    </body>
+</html>
