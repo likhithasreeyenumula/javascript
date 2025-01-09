55f62f3c618f8162208f8ed6fbe121d9d1c21f63
Likhitha sree <likhithasree1109@gmail.com>
2025-01-09 09:23:09 +0530

    Add files via upload

---
+++ 1/js111.js
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>111</title>
+        <script src=""></script>
+        <script>
+            $(document).ready(function(){
+                $("button").click(function(){
+                    $("p").css("background-color":"violet");
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <p style="background-color:#ff0000">The backgroundcolor of this paragraph is</p>
+    </body>
+</html>
