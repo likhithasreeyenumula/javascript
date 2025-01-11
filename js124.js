95ba2b7a18706722c3d69e65780de9df5acfe1c1
Likhitha sree <likhithasree1109@gmail.com>
2025-01-11 09:52:12 +0530

    Add files via upload

---
+++ js124.js
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>124</title>
+        <script src=" "></script>
+        <script>
+            $(document).ready(function(){
+                $("p").mouseleave(function(){
+                    $("p").css("background-color","blue");
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <p>Move your mouse cursor over this statement</p>
+    </body>
+</html>
