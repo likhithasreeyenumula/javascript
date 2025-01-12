1946983f9dd728ab42291866a325fda46cd085b8
Likhitha sree <likhithasree1109@gmail.com>
2025-01-12 11:32:40 +0530

    Add files via upload

---
+++ 1/js125.js
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>125</title>
+        <srcipt src=" "></srcipt>
+        <script>
+            $(document).ready(function(){
+                $("p").hover(function(){
+                    $(this).css("background-color","green");
+                },
+            function(){
+                $(this).css("background-color","voilet");
+            });
+            });
+        </script>
+    </head>
+    <body>
+        <p>hover your mouse pointer on me!</p>
+    </body>
+</html>
