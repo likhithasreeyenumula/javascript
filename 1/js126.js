5f24786ba424e22aa8ae428d78c3e290861357fc
Likhitha sree <likhithasree1109@gmail.com>
2025-01-12 11:33:02 +0530

    Add files via upload

---
+++ 1/js126.js
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>126</title>
+        <script src=" "></script>
+        <script>
+            $(document).ready(function(){
+                $("#h1").mousedown(function(){
+                    $("div").text("mouse down event triggered").show().fadeout(2000);
+                });
+            });
+        </script>
+    </head>
+</html>
