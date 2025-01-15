b89d2477165378a286e4cf346d8191187b2d3b8e
Likhitha sree <likhithasree1109@gmail.com>
2025-01-15 20:05:36 +0530

    Add files via upload

---
+++ 1/js129.js
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>129</title>
+        <srcipt src=" "></srcipt>
+        <script>
+            $(document).ready(function(){
+                $("p").mouse.over(function(){
+                    $("p").css("background-color","lightgreen");
+                });
+                $("p").mouseout(function(){
+                    $("p").css("background-color","green");
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <p>This is a mouse out </p>
+    </body>
+</html>
