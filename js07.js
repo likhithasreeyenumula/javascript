7f0990b39c6c905461cc64f429532e4a805cfeb2
Likhitha sree <likhithasree1109@gmail.com>
2026-02-07 08:08:45 +0530

    Update js07.js

--- js07.js
+++ js07.js
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>1</title>
 </head>
-<body>
+<body> 
     <script>
         a=2;b=5
         console.log("add:",(a+b))
